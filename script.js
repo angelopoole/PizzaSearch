a3252cad1558ad3ea3acbdbfe37e77b391d737d9
angelopoole <8thgenwolf@gmail.com>
2015-12-16 22:56:54 +0000

    four square api

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 $( document ).ready(function() {
  
- $( "submit" ).click(function() {
-  alert( "you have clicked" );
+ $( "#submit" ).click(function() {
+  //alert( "you have clicked" );
+ 
+  $.get("https://api.foursquare.com/v2/venues/search" +
+        "?client_id=" + "YYWZHVQ5HILD3IBYRQ0ZPFYGVITKCURIKIA0ORWLMXAIOHFH" +
+        "&client_secret=" + "DPFVKUQQGZBAUHNZRGHLBSPONVIIQUU0XLNJYF55M1Y2ZWWI" +
+        "&v=20151209" +
+        "&near=nyc" +
+        "&query=pizza",
+        function(data) {
+         var name=(data["response"]["venues"][0]["name"]); 
+         console.log(name);
+         $("#results").append(name);
+            // Callback code goes here. This gets executed after receiving data from the Foursquare API.
+        });
+ });
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 });
  
- 
- 
- });
